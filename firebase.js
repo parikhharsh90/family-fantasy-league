67a20daca61b55401cebfee71b1c824308243b77
parikhharsh90 <parikhharsh90@yahoo.com>
2026-03-31 10:54:02 -0700

    Refactor Firebase configuration and initialization
    Updated Firebase configuration to use placeholders for sensitive information and modified initialization method.

--- firebase.js
+++ firebase.js
@@ -1,21 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAe8KAbQxu-pbzFs_UybjxTcCSzHAwQAXQ",
-  authDomain: "family-fantasy-league-c62d0.firebaseapp.com",
-  projectId: "family-fantasy-league-c62d0",
-  storageBucket: "family-fantasy-league-c62d0.firebasestorage.app",
-  messagingSenderId: "484346285032",
-  appId: "1:484346285032:web:9d2e5fe52e37f5afd0325d",
-  measurementId: "G-GDCW1VWKFY"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+// Firestore Database
+const db = firebase.firestore();
