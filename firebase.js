526d0cdd393d7c08d291e863ab9f061c5fe79112
parikhharsh90 <parikhharsh90@yahoo.com>
2026-03-31 11:02:06 -0700

    Update Firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,13 +1,21 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyAe8KAbQxu-pbzFs_UybjxTcCSzHAwQAXQ",
+  authDomain: "family-fantasy-league-c62d0.firebaseapp.com",
+  projectId: "family-fantasy-league-c62d0",
+  storageBucket: "family-fantasy-league-c62d0.firebasestorage.app",
+  messagingSenderId: "484346285032",
+  appId: "1:484346285032:web:9d2e5fe52e37f5afd0325d",
+  measurementId: "G-GDCW1VWKFY"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Firestore Database
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
